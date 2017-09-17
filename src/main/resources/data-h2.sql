9bf9bba2eade6f6a19ab4041040c3b8643799284
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2017-09-17 01:59:47 -1000

    Fix sonar issues

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test1', 'news 1', 'https://www.service-public.pf/news1', 'DICP');
-INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test2', 'news 2', 'https://www.service-public.pf/news1', 'HC');
-INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test3', 'news 3', 'https://www.service-public.pf/news1', 'PR');
+INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test2', 'news 2', 'https://www.service-public.pf/news2', 'HC');
+INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test3', 'news 3', 'https://www.service-public.pf/news3', 'PR');
--- src/main/resources/data-postgresql.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test1', 'news 1', 'https://www.service-public.pf/news1', 'DICP');
-INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test2', 'news 2', 'https://www.service-public.pf/news1', 'HC');
-INSERT INTO NEWS (TITLE, DESCRIPTION, LINK, SERVICE) VALUES ('test3', 'news 3', 'https://www.service-public.pf/news1', 'PR');
